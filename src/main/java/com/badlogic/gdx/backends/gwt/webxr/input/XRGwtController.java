03498cc0ac54535736d7088649644d6a03c9fc1c
Yuri Pourre <yuripourre@gmail.com>
2022-04-28 10:59:52 -0700

    Handles controllers without vibration support

--- src/main/java/com/badlogic/gdx/backends/gwt/webxr/input/XRGwtController.java
+++ src/main/java/com/badlogic/gdx/backends/gwt/webxr/input/XRGwtController.java
@@ -1,9 +1,11 @@
 package com.badlogic.gdx.backends.gwt.webxr.input;
 
 import com.badlogic.gdx.backends.gwt.controllers.GamepadButton;
+import com.badlogic.gdx.backends.gwt.controllers.GamepadHapticActuator;
 import com.badlogic.gdx.backends.gwt.controllers.GwtController;
 import com.badlogic.gdx.math.Matrix4;
 import com.google.gwt.webxr.XRInputSource;
+import elemental2.core.JsArray;
 
 public class XRGwtController extends GwtController {
 
@@ -41,4 +43,22 @@ public class XRGwtController extends GwtController {
     public void setAxis(int axis, Double value) {
         inputSource.getGamepad().getAxes().setAt(axis, value);
     }
+
+
+    public GamepadHapticActuator getActuator() {
+        JsArray<GamepadHapticActuator> actuators = inputSource.getGamepad().getHapticActuators();
+        if (actuators == null || actuators.length == 0) {
+            return null;
+        }
+        return actuators.getAt(0);
+    }
+
+    private boolean hasActuators() {
+        JsArray<GamepadHapticActuator> actuators = inputSource.getGamepad().getHapticActuators();
+        if (actuators == null || actuators.length == 0) {
+            return false;
+        }
+        return true;
+    }
+
 }
