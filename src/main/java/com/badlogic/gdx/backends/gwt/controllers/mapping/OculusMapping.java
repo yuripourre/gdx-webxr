3b4e3005149ca7d3aecd63f33b0248864de89278
Yuri Pourre <yuripourre@gmail.com>
2022-04-30 18:47:19 -0700

    Add Oculus mapping

---
+++ src/main/java/com/badlogic/gdx/backends/gwt/controllers/mapping/OculusMapping.java
@@ -0,0 +1,20 @@
+package com.badlogic.gdx.backends.gwt.controllers.mapping;
+
+public class OculusMapping {
+
+    public static final int LEFT_TRIGGER = 0;
+    public static final int LEFT_SQUEEZE = 1;
+    // Unavailable
+    //public static final int BUTTON_ACTIONS = 2;
+    public static final int LEFT_THUMB_STICK = 3;
+    public static final int BUTTON_X = 4;
+    public static final int BUTTON_Y = 5;
+    public static final int RIGHT_TRIGGER = 0;
+    public static final int RIGHT_SQUEEZE = 1;
+    // Unavailable
+    //public static final int BUTTON_OCULUS = 2;
+    public static final int RIGHT_THUMB_STICK = 3;
+    public static final int BUTTON_A = 4;
+    public static final int BUTTON_B = 5;
+
+}
