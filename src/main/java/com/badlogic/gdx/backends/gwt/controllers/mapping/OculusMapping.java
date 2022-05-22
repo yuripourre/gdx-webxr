7fec6f48a96127d042fd238cfd14bfa31b458d01
Yuri Pourre <yuripourre@gmail.com>
2022-05-21 19:09:08 -0700

    Add axis mapping for oculus

--- src/main/java/com/badlogic/gdx/backends/gwt/controllers/mapping/OculusMapping.java
+++ src/main/java/com/badlogic/gdx/backends/gwt/controllers/mapping/OculusMapping.java
@@ -2,6 +2,12 @@ package com.badlogic.gdx.backends.gwt.controllers.mapping;
 
 public class OculusMapping {
 
+    // Reference: https://forums.oculusvr.com/t5/General-Development/WebXR-how-to-migrate-from-oculus-go-to-qwest/m-p/821517/highlight/true#M1731
+    public static final int AXIS_X_TOUCH_PAD = 0;
+    public static final int AXIS_Y_TOUCH_PAD = 1;
+    public static final int AXIS_X_JOYSTICK = 2;
+    public static final int AXIS_Y_JOYSTICK = 3;
+
     public static final int LEFT_TRIGGER = 0;
     public static final int LEFT_SQUEEZE = 1;
     // Unavailable
